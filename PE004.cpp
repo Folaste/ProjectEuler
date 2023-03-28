75a14a3f6fcfbe0c8bcf98ec97f0bc92e6aeb10e
Florian Leblanc <fleblanc@2F3.42angouleme.fr>
2023-03-28 18:28:48 +0200

    Solving problem 004 (Inconsistent)

---
+++ PE004.cpp
@@ -0,0 +1,62 @@
+// Find the biggest palindrome made for the product of two 3-digit numbers
+
+#include <iostream>
+#include <sstream>
+
+int	ft_intlen(int nb)
+{
+	int i = 1;
+
+	while (nb >= 10)
+	{
+		i++;
+		nb /= 10;
+	}
+
+	return (i);
+}
+
+const char*	ft_itoa(int nb)
+{
+	std::stringstream	ss;
+	ss << nb;
+	return (ss.str().c_str());
+}
+
+bool	ft_is_palindrome(int nb)
+{
+	const char*	str;
+	int			len;
+	
+	len = ft_intlen(nb);
+	str = ft_itoa(nb);	
+	for (int i = 0; i < len / 2 ; i++)
+	{
+		if (str[i] != str[len - 1 - i])
+			return (false);
+	}
+	return (true);
+}
+
+int	main(void)
+{
+	int	result,
+		biggest;
+
+	for (int i = 100 ; i < 1000 ; i++)
+	{
+		for (int j = 100 ; j < i + 1 ; j++)
+		{
+			result = i * j;
+			if (ft_is_palindrome(result) == true && result > biggest)
+			{
+				std::cout << i << " " << j << std::endl;
+				biggest = result;
+			}
+		}
+	}
+
+	std::cout << "The biggest palindrome is " << biggest << std::endl;
+
+	return (0);
+}
