f6a8c3ea927260786bc9b4ffb960499366fc2848
Florian Leblanc <fleblanc@2C4.42angouleme.fr>
2023-03-30 16:48:07 +0200

    Solving problem 010

---
+++ PE010.cpp
@@ -0,0 +1,39 @@
+//Find the sum of all prime numbers below 2000000
+
+#include <iostream>
+
+int	ft_sqrt(int nb)
+{
+	int i = 0;
+	if (2147395600 <= nb && nb <= 2147483647)
+		return (46340);
+	while (i * i <= nb)
+		i++;
+	return (--i);
+}
+
+bool	ft_is_prime(int nb)
+{
+	int	sqrt;
+
+	if (nb <= 1)
+		return (false);
+	sqrt = ft_sqrt(nb);
+	for (int i = 2 ; i <= sqrt; i++)
+		if (nb % i == 0)
+			return (false);
+	return (true);
+}
+
+int	main(void)
+{
+	unsigned long	sum = 0;
+	
+	for (int nb = 1; nb < 2000000 ; nb++)
+		if (ft_is_prime(nb) == true)
+			sum += nb;
+
+	std::cout << "The sum is " << sum << std::endl;
+	
+	return (0);
+}
