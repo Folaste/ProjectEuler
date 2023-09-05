37a3eda7e6983a297e5018ce680375459557dcee
Folaste <leblancflo@orange.fr>
2023-09-05 16:01:25 +0200

    Nicer way to solve but less optimized

--- PE005.cpp
+++ PE005.cpp
@@ -15,14 +15,17 @@ int	main(void)
 	for (int i = 2; i <= 20; i++)
 		max *= i;
 
-	for (unsigned long long j = 20 ; j < max; j++)
+	for (unsigned long long i = 20 ; i < max; i++)
 	{
-		//Find a way to do it more nicely
-		if (j % 2 == 0 && j % 3 == 0 && j % 4 == 0 && j % 5 == 0 && j % 6 == 0 && j % 7 == 0 && j % 8 == 0 && j % 9 == 0 && j % 10 == 0 && j % 11 == 0 && j % 12 == 0 && j % 13 == 0 && j % 14 == 0
-			&& j % 15 == 0 && j % 16 == 0 && j % 17 == 0 && j % 18 == 0 && j % 19 == 0 && j % 20 == 0)
+		for (int j = 2; j <= 20; j++)
 		{
-			std::cout << "This number is " << j << std::endl;
-			break;
+			if (i % j != 0)
+				break;
+			if (j == 20)
+			{
+				std::cout << "This number is " << i << std::endl;
+				return (0);
+			}
 		}
 	}
 	
