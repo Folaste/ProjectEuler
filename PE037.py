6593b8bd8c17294db7acc8a71401e0eee3648260
Folaste <leblancflo@orange.fr>
2024-06-05 17:46:23 +0200

    Optimized problem 037

--- PE037.py
+++ PE037.py
@@ -9,41 +9,45 @@ Problem 037: Truncatable primes
 
     NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 
-    Solved in 1427 ms.
+    Solved in 1427 ms (1st version), in 172 ms (2nd version).
 """
 
 import time
 
 
-def is_prime(number: int) -> bool:
-    if number < 2:
-        return False
-    if number == 2:
-        return True
-    if number % 2 == 0:
-        return False
-    for mod in range(3, int(number ** 0.5) + 1, 2):
-        if number % mod == 0:
-            return False
-    return True
+def eratosthenes_sieve(limit: int) -> list[bool]:
+    table = [True] * limit
+    table[0:2] = [False, False]
+    for ind, val in enumerate(table):
+        if val:
+            table[ind * 2::ind] = [False] * (((limit - 1) // ind) - 1)
+    return table
 
 
 start = time.time_ns()
-test_number = 11
-prime_list = []
+sieve = eratosthenes_sieve(1_000_000)
 
-while len(prime_list) != 11:
-    if is_prime(test_number):
-        truncatable = True
-        for i in range(1, len(str(test_number))):
-            if not is_prime(int(str(test_number)[i:])) or not is_prime(int(str(test_number)[:-i])):
-                truncatable = False
-                break
-        if truncatable:
-            prime_list.append(test_number)
-    test_number += 2
 
-result = sum(prime_list)
+def is_prime(n: int) -> bool:
+    return sieve[n]
+
+
+prime_list = [i for i, val in enumerate(sieve) if val and i > 10]  # 2, 3, 5, 7 are not considered truncatable primes
+result_list = []
+for test_number in prime_list:
+    str_test_number = str(test_number)
+    truncatable = True
+    for i in range(1, len(str_test_number)):
+        if not is_prime(int(str_test_number[i:])) or not is_prime(int(str_test_number[:-i])):
+            truncatable = False
+            break
+    if truncatable:
+        result_list.append(test_number)
+        if len(result_list) == 11:
+            break
+
+
+result = sum(result_list)
 end = time.time_ns()
 
 print(__doc__)
